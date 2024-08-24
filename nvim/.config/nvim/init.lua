b0238f07b3a6fffc7c94553593d8f42128900630
chrisaddy <chris.william.addy@gmail.com>
2024-08-23 21:54:27 -0400

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -115,7 +115,6 @@ require('lazy').setup({
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
-
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
@@ -123,9 +122,6 @@ require('lazy').setup({
       statusline.section_location = function()
         return '%2l:%-2v'
       end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
   { import = 'plugins' },
